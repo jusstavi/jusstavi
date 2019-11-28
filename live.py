d628081805e3af7fb360ea7058ecb085a902d37b
jusstavi <56784418+jusstavi@users.noreply.github.com>
2019-11-28 10:03:40 +0530

    Add files via upload

---
+++ live.py
@@ -0,0 +1,21 @@
+import time
+
+Heart = ["Soukya"]
+
+def live():
+	
+	if 'Soukya' in Heart:
+		breathe()
+		
+	else:
+		print("No reasons to live...")
+		
+def breathe():
+	while True:
+		print("Inhaling for you...\n")
+		time.sleep(2)
+		print("Exhaling for you...\n")
+		time.sleep(2)
+		
+if __name__ == '__main__':
+	live()
